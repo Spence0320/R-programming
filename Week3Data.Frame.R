4e93150e48ed5ce16840793ca4b124139c248e5a
Spence0320 <spencer.anstead.jaacks@gmail.com>
2025-02-02 22:54:54 -0500

    Add files via upload

---
+++ Week3Data.Frame.R
@@ -0,0 +1,7 @@
+Name = c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+
+ABC_results = c(4, 62, 51, 21, 2, 14, 15)
+CBS_results = c(12, 75, 43, 19, 1, 21, 19)
+average = c((ABC_results+CBS_results)/2)
+results = data.frame(Name, ABC_results, CBS_results,average)
+results
